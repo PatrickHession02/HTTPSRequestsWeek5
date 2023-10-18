8d896bc26cbd7844841e4d4aa580f61332926dc4
G00394619@atu.ie <g00394619@atu.ie>
2023-10-18 09:36:29 +0100

    Initial commit

---
+++ src/main/java/ie/atu/httpsrequests/HttpsRequestsApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.httpsrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsRequestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/httpsrequests/HttpsRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.httpsrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
